2c02a564e16bc50596b93659327c1bcc05dedc0d
mikalai.yurkin <mikalai.yurkin@leverx.com>
2015-03-03 20:37:17 +0300

    tests updated, passed

--- tests/tests.py
+++ tests/tests.py
@@ -1,35 +1,35 @@
 from pygibson import PyGibsonError
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 from django.conf import settings
 settings.configure()
 
 
 def set_connection():
 
-    settings.SESSION_GIBSON_TIMEOUT = 100
-
     try:
-        # set IPC connection settings
-        settings.SESSION_GIBSON_UNIX_SOCKET = '/var/run/gibson.sock'
-        from gibson_sessions.sessions import SessionStore
-        ss = SessionStore()
+        settings.SESSION_GIBSON_TIMEOUT = 100
+
+        try:
+            # set IPC connection settings
+            settings.SESSION_GIBSON_UNIX_SOCKET = '/var/run/gibson.sock'
+            from gibson_sessions.sessions import SessionStore
+            ss = SessionStore()
+        except PyGibsonError:
+            # set TCP connection settings
+            settings.SESSION_GIBSON_HOST = '127.0.0.0'
+            settings.SESSION_GIBSON_PORT = 10128
+            from gibson_sessions.sessions import SessionStore
+            ss = SessionStore()
+
+        ss.session_backend.ping()
+        return ss
     except PyGibsonError:
-        # set TCP connection settings
-        settings.SESSION_GIBSON_HOST = '127.0.0.0'
-        settings.SESSION_GIBSON_PORT = 10128
-        from gibson_sessions.sessions import SessionStore
-        ss = SessionStore()
-
-    ss.session_backend.ping()
-    return ss
-
-
-def connection_test():
-    set_connection()
+        return None
 
 
 def modification_test():
     storage = set_connection()
+    ok_(storage)
     eq_(storage.modified, False)
     storage['gibson'] = 'modified'
     eq_(storage.modified, True)
@@ -38,7 +38,7 @@ def modification_test():
 
 def create_and_delete_test():
     storage = set_connection()
-    storage['gibson'] = 'modified'
+    ok_(storage)
     storage.save()
     eq_(storage.exists(storage.session_key), True)
     storage.delete()
@@ -47,7 +47,7 @@ def create_and_delete_test():
 
 def create_and_delete_locked_test():
     storage = set_connection()
-    storage['gibson'] = 'modified'
+    ok_(storage)
     storage.save()
     eq_(storage.exists(storage.session_key), True)
     storage.session_backend.lock(storage.prefixed_key_name(storage.session_key), 60)
@@ -55,5 +55,24 @@ def create_and_delete_locked_test():
     eq_(storage.exists(storage.session_key), False)
 
 
-def delete_test():
-    pass
+def update_locked_test():
+    storage = set_connection()
+    ok_(storage)
+    storage.save()
+    eq_(storage.exists(storage.session_key), True)
+    storage['gibson'] = 'before_lock'
+    storage.session_backend.lock(storage.prefixed_key_name(storage.session_key), 60)
+    storage['gibson'] = 'ignore_lock'
+    storage.save()
+    eq_(storage['gibson'], 'ignore_lock')
+
+
+def expiration_test():
+    import time
+    storage = set_connection()
+    ok_(storage)
+    storage.set_expiry(1)
+    storage.save()
+    eq_(storage.exists(storage.session_key), True)
+    time.sleep(2)
+    eq_(storage.exists(storage.session_key), False)
